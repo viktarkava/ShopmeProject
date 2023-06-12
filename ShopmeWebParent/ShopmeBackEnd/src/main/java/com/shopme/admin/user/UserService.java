03c17fa4e3b954789367801fea338270698fad90
Viktar Kavalenka <viktar.kava@gmail.com>
2023-06-12 22:37:40 +0300

    finish New User function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Service
@@ -12,8 +13,19 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> listAll(){
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	public List<User> listAll() {
 		return (List<User>) userRepository.findAll();
+	}
+
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepository.findAll();
+	}
+
+	public void save(User user) {
+		userRepository.save(user);
 	}
 }
