e4c660785cd2e394a239e9ab26be25e400526b0f
Viktar Kavalenka <viktar.kava@gmail.com>
2023-06-22 15:55:35 -0400

    delete in UserService

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -73,4 +73,12 @@ public class UserService {
 			throw new UserNotFoundException("Could not find any user with ID " + id);
 		}
 	}
+
+	public void delete(Integer id) throws UserNotFoundException {
+		Long countById = userRepository.countById(id);
+		if (countById == null || countById == 0) {
+			throw new UserNotFoundException("Could not find any user with ID " + id);
+		}
+		userRepository.deleteById(id);
+	}
 }
